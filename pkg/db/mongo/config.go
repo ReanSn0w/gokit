12e110b2ec01a58463c8bd314c9623c06fc8e8c3
Dmitriy Papkov <papkovda@me.com>
2024-08-26 16:51:18 +0300

    [Add] mongo package

---
+++ pkg/db/mongo/config.go
@@ -0,0 +1,29 @@
+package mongo
+
+import (
+	"context"
+	"os"
+
+	"github.com/go-pkgz/lgr"
+)
+
+type Config struct {
+	Mongo struct {
+		URI      string `long:"url" env:"URL" default:"http://127.0.0.1:27017" description:"monogo connection url"`
+		Database string `long:"database" env:"DATABASE" default:"Application" description:"monogo database name"`
+	} `group:"mongo" namespace:"mongo" env-namespace:"MONGO"`
+}
+
+func (c Config) ConnectMongoDB(ctx context.Context, log lgr.L) (*Mongo, error) {
+	return New(ctx, log, c.Mongo.URI, c.Mongo.Database)
+}
+
+func (c Config) MustConnectMongoDB(ctx context.Context, log lgr.L) *Mongo {
+	mongo, err := c.ConnectMongoDB(ctx, log)
+	if err != nil {
+		lgr.Default().Logf("[ERROR] mongo connection error: %v", err)
+		os.Exit(2)
+	}
+
+	return mongo
+}
