a63d4875cfcdea2b9c45b18fc16b48a7049e6715
Dmitriy Papkov <papkovda@me.com>
2025-03-07 11:41:05 +0300

    [Add] json middleware package
    - Добавл пакет для получения данных из JSON тела запроса

---
+++ pkg/web/mv/json/mv.go
@@ -0,0 +1,44 @@
+package json
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+
+	"github.com/ReanSn0w/gokit/pkg/web"
+)
+
+var (
+	jsonDecoderCtxKey = &jsonDecoderCtx{}
+)
+
+type jsonDecoderCtx struct{}
+
+type Validate interface {
+	Validate() error
+}
+
+func Decoder[T any](h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var data T
+		err := json.NewDecoder(r.Body).Decode(&data)
+		if err != nil {
+			web.NewResponse(err).Write(http.StatusBadRequest, w)
+			return
+		}
+
+		if validate, ok := any(data).(Validate); ok {
+			if err := validate.Validate(); err != nil {
+				web.NewResponse(err).Write(http.StatusBadRequest, w)
+				return
+			}
+		}
+
+		ctx := context.WithValue(r.Context(), jsonDecoderCtxKey, &data)
+		h.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func Get[T any](ctx context.Context) *T {
+	return ctx.Value(jsonDecoderCtxKey).(*T)
+}
